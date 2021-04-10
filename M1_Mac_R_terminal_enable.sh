6fc2e58a838a8c461264d983e5ff770240fab41e
MOBioJapan <81572968+MOBioJapan@users.noreply.github.com>
2021-04-10 13:06:31 +0900

    Update M1_Mac_R_terminal_enable.sh

--- M1_Mac_R_terminal_enable.sh
+++ M1_Mac_R_terminal_enable.sh
@@ -1,5 +1,5 @@
 #!usr/bin/zsh
-#M1_Mac_Rターミナルでの起動するためのalias移動
+#M1_Mac_Rターミナルで起動するためのalias移動
 ln -s /Library/Frameworks/R.framework/Versions/Current/Resources/bin/R /usr/local/bin
 ln -s /Library/Frameworks/R.framework/Versions/Current/Resources/bin/Rscript /usr/local/bin
 done
